124571bba53b641a907ee920c8d93623f10e9ae1
hiroworks <hatawork@gmail.com>
2025-08-03 12:39:43 +0900

    Clean and commit project with .gitignore

--- App.tsx
+++ App.tsx
@@ -1,11 +1,2 @@
 // App.tsx
-import { Slot } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <Slot />
-    </SafeAreaProvider>
-  );
-}
+import "expo-router/entry";
---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  console.log("✅ Layout 起動");
+  return <Stack />;
+}

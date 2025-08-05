a0eaedff6a74721c55a78bb04ef8d877a4da8cf6
hiroworks <hatawork@gmail.com>
2025-08-05 17:36:58 +0900

    State before building method posted on website

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@
 import { View, Text } from 'react-native';
 
 export default function Home() {
+  console.log("✅ Home component loaded");  // ← ここ追加
+
   return (
     <View>
       <Text>Hello, world!</Text>
